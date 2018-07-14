c08ca1d50329ff539ed2d31eb0aae74a3d257694
Alexandre Kone <kone.alexandr@gmail.com>
2018-07-14 13:40:04 +0200

    make it better

--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => res.render('blog/index'));
-router.get('/:post_id', (req, res, next) => res.render('blog/detail'));
+router.get('/:slug', (req, res, next) => res.render('blog/detail'));
 
 module.exports = router;
--- routes/country.js
+++ routes/country.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/:country', (req, res, next) => res.render('country'));
+router.get('/', (req, res, next) => {
+  res.render('pages/country/index')
+})
+router.get('/:country', (req, res, next) => {
+  res.render('pages/country/details', { country: req.params.country})
+});
 
 module.exports = router;
